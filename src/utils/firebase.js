24196f0d03e10e04b84c2c54633ac5b893e3faf7
Robert Weilbaecher <robertgweilbaecher@gmail.com>
2016-11-07 10:51:34 -0600

    initial commit

---
+++ src/utils/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyB87Fr0F0dicx_65VnzviYs7Doe5EI-L74",
+  authDomain: "growlrr-b210e.firebaseapp.com",
+  databaseURL:"https://growlrr-b210e.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "278457323900"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
