5bd20db62310600b15737616f84cd8a3d7d105ea
Joseph Callaway <jeauxcal@gmail.com>
2016-11-07 11:28:19 -0600

    Changed firebase database to growlrr

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 
 const config = {
-    apiKey: "AIzaSyCvldY8SQJDbKURgiBZqIVOeGm0XFb3e8g",
-    authDomain: "ga-todo-list-46567.firebaseapp.com",
-    databaseURL: "https://ga-todo-list-46567.firebaseio.com",
-    storageBucket: "ga-todo-list-46567.appspot.com",
-    messagingSenderId: "769545845017"
+    apiKey: "AIzaSyB87Fr0F0dicx_65VnzviYs7Doe5EI-L74",
+    authDomain: "growlrr-b210e.firebaseapp.com",
+    databaseURL: "https://growlrr-b210e.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "278457323900"
   };
 
 firebase.initializeApp(config);
